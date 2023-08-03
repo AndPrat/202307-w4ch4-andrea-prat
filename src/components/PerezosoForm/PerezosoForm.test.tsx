2430919236074416beaf1b5809d714f0a40889d0
Andrea Prat <andr8ide@gmail.com>
2023-08-03 23:35:31 +0200

    Change the language of project and test, change structure of form

--- src/components/PerezosoForm/PerezosoForm.test.tsx
+++ src/components/PerezosoForm/PerezosoForm.test.tsx
@@ -3,8 +3,8 @@ import PerezosoForm from "./PerezosoForm";
 
 describe("Given a PerezosoForm component", () => {
   describe("When it is rendered", () => {
-    test("The it should show button with name 'Create a Perezoso!'", () => {
-      const nameButton = "Create a Perezoso!";
+    test("The it should show button with name 'Crea tu Perezoso!'", () => {
+      const nameButton = "Crea tu Perezoso!";
 
       render(<PerezosoForm />);
       const buttonPerezosoForm = screen.getByRole("button", {
@@ -14,8 +14,8 @@ describe("Given a PerezosoForm component", () => {
       expect(buttonPerezosoForm).toBeInTheDocument();
     });
 
-    test("The it should show a label with name 'Perezoso name:'", () => {
-      const nameLabel = "Perezoso name:";
+    test("The it should show a label with name 'Nombre del Perezoso:'", () => {
+      const nameLabel = "Nombre del Perezoso:";
 
       render(<PerezosoForm />);
       const nameLabelPerezosoForm = screen.getByLabelText(nameLabel);
--- src/components/PerezosoForm/PerezosoForm.tsx
+++ src/components/PerezosoForm/PerezosoForm.tsx
@@ -1,13 +1,29 @@
+import "./PerezosoForm.css";
+
 const PerezosoForm = (): React.ReactElement => {
   return (
-    <form action="">
-      <label htmlFor="name">Perezoso name:</label>
-      <input type="text" id="name" />
-      <label htmlFor="url">Url Perezoso picture:</label>
-      <input type="text" id="url" />
-      <label htmlFor="fingers">Number of fingers:</label>
-      <input type="number" id="fingers" />
-      <button>Create a Perezoso!</button>
+    <form className="perezoso-form">
+      <div className="perezoso-form__group">
+        <label htmlFor="name" className="perezoso-form__label">
+          Nombre del Perezoso:
+        </label>
+        <input type="text" id="name" className="perezoso-form__input" />
+      </div>
+      <div className="perezoso-form__group">
+        <label htmlFor="url" className="perezoso-form__label">
+          Url imagen de Perezoso:
+        </label>
+        <input type="text" id="url" className="perezoso-form__input" />
+      </div>
+      <div className="perezoso-form__group">
+        <label htmlFor="fingers" className="perezoso-form__label">
+          Numero de dedos:
+        </label>
+        <input type="number" id="fingers" className="perezoso-form__input" />
+      </div>
+      <div className="perezoso-form__group">
+        <button className="perezoso-form__button">Crea tu Perezoso!</button>
+      </div>
     </form>
   );
 };
